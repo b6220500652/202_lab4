ef445e82bc7e492763f6f563557e8b985c03a523
b6220500652 <narisara.kaa@ku.th>
2021-03-04 17:48:55 +0700

    เปลี่ยนเลขข้อ

---
+++ 6220500652_4_3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i,n;
+	char x[200];
+	scanf("%s",x);
+	n=strlen(x);
+	for(i=0;i<n;i++)
+    {
+       if(x[i]==x[i+1])
+       {
+           continue;
+       }
+       else if(x[i]!=x[i+1])
+       {
+           printf("%c",x[i]);
+       }
+    }	
+}

6820fb2c40e89be668dd1f89f21cf260631e7750
b6220500652 <narisara.kaa@ku.th>
2021-03-04 17:59:03 +0700

    1

--- 6220500652_4_1.c
+++ 6220500652_4_1.c
@@ -2,19 +2,35 @@
 #include<string.h>
 int main()
 {
-	int i,n;
-	char x[200];
-	scanf("%s",x);
-	n=strlen(x);
-	for(i=0;i<n;i++)
+    int n,i,j;
+    scanf("%d",&n);
+    int x[n][3],y[n-2],z[3]={4,2,1};
+    for(i=0;i<n;i++)
     {
-       if(x[i]==x[i+1])
-       {
-           continue;
-       }
-       else if(x[i]!=x[i+1])
-       {
-           printf("%c",x[i]);
-       }
-    }	
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&x[i][j]);
+        }
+    }
+   for (i=0;i<n-2;i++)
+    {
+        y[i]=0;
+        for (j=0;j<3;j++)
+        {
+            y[i]+=(x[i][j]+x[i+1][j]+x[i+2][j])*z[j];
+        }
+    }
+    int max=0,min=0;
+    for (i=1;i<n-2;i++)
+    {
+        if(y[i]>y[max])
+        {
+            max=i;
+        }
+        else if(y[i]<y[min])
+        {
+            min=i;
+        }
+    }
+    printf("%d %d",max+1,min+1);
 }

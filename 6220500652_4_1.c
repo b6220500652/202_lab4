86d7b2ad4914c177cab340d0026861652200f665
b6220500652 <narisara.kaa@ku.th>
2021-03-04 15:56:05 +0700

    finish

--- 6220500652_4_1.c
+++ 6220500652_4_1.c
@@ -5,5 +5,16 @@ int main()
 	int i,n;
 	char x[200];
 	scanf("%s",&x);
-	
+	n=strlen(x);
+	for(i=0;i<n;i++)
+    {
+       if(x[i]==x[i+1])
+       {
+           continue;
+       }
+       else if(x[i]!=x[i+1])
+       {
+           printf("%c",x[i]);
+       }
+    }	
 }
